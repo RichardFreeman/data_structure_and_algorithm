2b92e4f45785af71500dc8e55c2bce9efc7a526e
Richard Huang <richard.huang@activenetwork.com>
2015-08-13 12:56:10 +0800

    python3 subgenertor permutation

--- permutation/permutation.py
+++ permutation/permutation.py
@@ -3,14 +3,22 @@ def swap(arr, x, y):
     arr[x] = arr[y]
     arr[y] = tmp
 
-def permute(arr, s, e):
+def permute0(arr, s, e):
     if(s == e):
         print(arr)
-
     else:
-        for i in range(s, e):
+        for i in range(s, (e+1)):
             swap(arr, s, i)
             permute(arr, s+1, e)
             swap(arr, s, i)
 
-permute([1,2,3], 0, 3)
+
+# only for Python3, subgenertor
+def permute1(arr, s, e):
+    if(s == e):
+        yield arr
+    else:
+        for i in range(s, (e+1)):
+            swap(arr, s, i)
+            yield from permute(arr, s+1, e)
+            swap(arr, s, i)
