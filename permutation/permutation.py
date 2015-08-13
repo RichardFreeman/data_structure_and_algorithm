cc2292a9cf2104af02274c00f9f67edbc30c8ed2
Richard Huang <richard.huang@activenetwork.com>
2015-08-13 15:19:23 +0800

    serval yield methods

--- permutation/permutation.py
+++ permutation/permutation.py
@@ -13,7 +13,7 @@ def permute0(arr, s, e):
             swap(arr, s, i)
 
 
-# only for Python3, subgenertor
+# only for Python3, subgenerator
 def permute1(arr, s, e):
     if(s == e):
         yield arr
@@ -22,3 +22,24 @@ def permute1(arr, s, e):
             swap(arr, s, i)
             yield from permute(arr, s+1, e)
             swap(arr, s, i)
+
+# not accumulator
+def permute2(arr):
+    if len(arr) == 1:
+        yield arr
+    else:
+        for i in range(len(arr)):
+            for perm in permute2(arr[:i] + arr[i+1:]):
+                yield [arr[i]] + perm
+
+def permute3(arr, prefix):
+    if len(arr) == 1:
+        yield arr + prefix
+    else:
+        for i in range(len(arr)):
+            yield from permute3(arr[:i] + arr[i+1:], prefix+[arr[i]])
+
+
+[x for x in permute1([1,2,3], 0, 2)]
+[y for y in permute2([1,2,3])]
+[z for z in permute3([1,2,3])]
